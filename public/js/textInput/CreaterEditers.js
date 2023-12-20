19d41372a9b2543e3a4329d81b97b4d558c73b48
Tobito45 <borya.od@gmail.com>
2023-12-20 11:56:51 +0100

    Remove paragraphs

--- public/js/textInput/CreaterEditers.js
+++ public/js/textInput/CreaterEditers.js
@@ -2,12 +2,10 @@ import {TextEditObj} from "./TextEditObj.js";
 import {TextAreaEditObj} from "./TextAreaEditObj.js";
 import {InputImageObj} from "../imageInput/InputImageObj.js";
 import {GalleryEditor} from "../imageInput/GalleryEditor.js";
-import {ParagraphAdd} from "../paragraphAndCapitol/ParagraphAdd.js";
 
 
 class CreaterEditers {
     static #countTextEdit = 12;//15;
-    static #countTextAreaEdit = 5;
     static #countImageEdit = 1//4;
     constructor() {
         for(let i = 1; i <= CreaterEditers.#countTextEdit; i++) {
@@ -17,9 +15,6 @@ class CreaterEditers {
         }
 
         new TextAreaEditObj(1);
-     //   for(let i = 1; i <= CreaterEditers.#countTextAreaEdit; i++) {
-      //      new TextAreaEditObj(i)
-       // }
 
         for(let i = 1; i <= CreaterEditers.#countImageEdit; i++) {
            new InputImageObj(i)
